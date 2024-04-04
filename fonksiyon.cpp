55f173c81da69efebb0dabdc90c4f935d92eaf1e
python533 <143389231+python533@users.noreply.github.com>
2024-04-04 10:57:19 +0300

    C++ Fonksiyon
    i<t{0<t<0
         {1 0 <t<1
         {t t>1

---
+++ fonksiyon.cpp
@@ -0,0 +1,24 @@
+using namespace std;
+
+int main(){
+  setlocale(LC_ALL,"Turkish");
+  int t;
+  cout<<"Bir sayı Girin:";
+  cin>>t;
+
+  if (t<0) {
+  for ( i = 0; i < 4; i++) {
+    if (i=1) {
+      if (i=t) {
+       continue
+      }
+    }
+  }
+
+}else
+std::cout << "Fonksiyon'daki i değeri" << i;
+
+
+
+
+}
